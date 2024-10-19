e33eb3559e40636d486cdc06122577ad54777f8f
emfallat <fallat.e@northeastern.edu>
2024-10-19 13:27:06 -0400

    About page Updates Liz

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { chakra, Input, Stack } from '@chakra-ui/react';
+import { Center, Square, Circle } from '@chakra-ui/react';
+import {Box, AbsoluteCenter} from '@chakra-ui/react';
 
 export default function About() {
     return (
-        <div>
-            <h1>About page!</h1>
-        </div>
+        <div style={{ backgroundColor: 'lightblue', width: '1390px', height: '500px' }}>
+            <AbsoluteCenter>
+                <p style={{ color: 'white' }}>About page!</p>
+            </AbsoluteCenter>
+      </div>
     );
 
 }
