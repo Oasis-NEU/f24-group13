fba2498b9ca2a213cac344a2daeb39da77d1a198
emfallat <fallat.e@northeastern.edu>
2024-10-27 13:31:53 -0400

    Updated profile page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,9 @@ import { chakra, Input, Stack } from '@chakra-ui/react';
 import {Box, AbsoluteCenter, useDisclosure} from '@chakra-ui/react';
 import { Button, ButtonGroup } from '@chakra-ui/react'
 import NavigationBar from '../components/NavigationBar';
+import { Image } from "@chakra-ui/react";
+import { Link } from 'react-router-dom';
+import { Flex, Grid, Text } from '@chakra-ui/react';
 import {
     Drawer,
     DrawerBody,
@@ -44,20 +47,78 @@ export default function About() {
                     </DrawerContent>
                     </Drawer> 
                         </div>
+            <div style={{ backgroundColor: '#E9EDC9', width: '100%', height: '25px', 
+                display: 'flex', justifyContent: 'left',
+                flexDirection: 'column'}}>
+                    <Image
+                        height="25px"
+                        width = "90%"
+                        src='test-images/Main Picture.png'
+                    />
+            </div>
             <div style={{ backgroundColor: '#E9EDC9', width: '100%', height: '500px', 
                 display: 'flex', alignItems: 'center', justifyContent: 'center' ,
                 flexDirection: 'column'}}>
                     <p style={{ color: 'white', fontSize: '60px' }}>Style Swap</p>
                     <p></p>
                     <p style={{ color: 'white', fontSize: '18px' }}>our slogan</p>
+                    {/*Links to the sign in page  */}
+                    <Link to={'/signin'}> <Text color = "white"> Sign up! </Text></Link> 
             </div>
-            <div style={{ backgroundColor: 'white', width: '100%', height: '300px',
+            <div style={{ backgroundColor: 'white', width: '100%', height: '500px',
                 display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <p style={{ color: 'black' }}>This is where our blurb will go.</p>
+                    <p style={{ color: 'black' }}>Blurb about Closet Swap</p>
             </div>
-            <div style={{ backgroundColor: '#D4A373', width: '100%', height: '500px',
+            <div>
+                    <stack>
+                    <div style={{ backgroundColor: '#D4A373', width: '100%', height: '300px',
                 display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                    <p style={{ color: 'white' }}>This is where our pictures and about us will go.</p>
+                    <Image
+                        height="200px"
+                        src='test-images/Stock img.jpg'
+                    />
+                    <p style={{ color: 'white' }}>My name is Liz and I am a third year Computer Engineering and 
+                        Comuter Science major. ahdusiufehwebfkj;wherihwekrndfjfopuwoiefhwfnwfnwfjwherfuiwyroqjlrnNR
+                        EUIRHWeiurhBRKhrfiuhfenwfJNEFIWEUR8WYRWEBFSFN;efsh
+                    </p>
+                    </div>
+                    <div style={{ backgroundColor: '#D4A373', width: '100%', height: '300px',
+                display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+                    <p style={{ color: 'white' }}>My name is kyra and I am Liz's twin and I am a third year Computer Engineering and 
+                        Comuter Science major. ahdusiufehwebfkj;wherihwekrndfjfopuwoiefhwfnwfnwfjwherfuiwyroqjlrnNR
+                        EUIRHWeiurhBRKhrfiuhfenwfJNEFIWEUR8WYRWEBFSFN;efsh
+                    </p>
+                    <Image
+                        height="200px"
+                        src='test-images/Stock img.jpg'
+                    />
+                    </div>
+                    <div style={{ backgroundColor: '#D4A373', width: '100%', height: '300px',
+                display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <Image
+                        height="200px"
+                        src='test-images/Stock img.jpg'
+                    />
+                    <p style={{ color: 'white' }}>My name is Liz and I am a third year Computer Engineering and 
+                        Comuter Science major. ahdusiufehwebfkj;wherihwekrndfjfopuwoiefhwfnwfnwfjwherfuiwyroqjlrnNR
+                        EUIRHWeiurhBRKhrfiuhfenwfJNEFIWEUR8WYRWEBFSFN;efsh
+                    </p>
+                    </div>
+                    <div style={{ backgroundColor: '#D4A373', width: '100%', height: '300px',
+                display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+
+                    <p style={{ color: 'white' }}>My name is kyra and I am Liz's twin and I am a third year Computer Engineering and 
+                        Comuter Science major. ahdusiufehwebfkj;wherihwekrndfjfopuwoiefhwfnwfnwfjwherfuiwyroqjlrnNR
+                        EUIRHWeiurhBRKhrfiuhfenwfJNEFIWEUR8WYRWEBFSFN;efsh
+                    </p>
+                    <Image
+                        height="200px"
+                        src='test-images/Stock img.jpg'
+                    />
+                    </div>
+                    </stack>
+
             </div>
         </div>
         
