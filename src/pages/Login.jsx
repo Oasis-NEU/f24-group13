d8350b3663e835f9c37317875676c4934d84b17c
Khushi Khan <76977629+khushikhan0@users.noreply.github.com>
2024-11-21 01:29:50 -0500

    login / signup page styling

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
-import { Flex, Grid, Text, Button, Input, useDisclosure } from '@chakra-ui/react';
+import { Flex, Grid, Text, Button, Input, useDisclosure, background, textDecoration } from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
 import { supabase, profiles, userposts } from '../database/db';
-import NavigationBar from '../components/NavigationBar';
-import MenuButton from '../components/MenuButton';
+import backgroundImage from "../../test-images/gradient_bkgd.png";
 
 export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [errorMsg, setErrorMsg] = useState('');
     const navigate = useNavigate();
+
+    const backgroundStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+    };
+
+    const inputBoxStyle = {
+        borderRadius: "25px",
+        background: "#FEFAE0",
+        padding: "20px",
+    };
 
     async function validateData() {
         if (username == '' || password == '') {
@@ -43,30 +56,40 @@ export default function Login() {
     } 
 
     return (
-        <Flex direction="column" align="center" justify="center" height="100vh" padding={4}>
+        <Flex direction="column" align="center" justify="center" height="100vh" padding={4} style={backgroundStyle}>
             <Grid 
                 templateColumns="repeat(1, 1fr)" // creating 1 column, column will take up full available width 
                 gap={4} // space between grid items vertically and horizontally
                 width="100%" // sets the width of the grid container to 100% of the flex container's width
                 maxWidth="400px" // limits how wide the grid can become
                 justifyItems="center" // centers the content of each grid cell horizontally
+                style={inputBoxStyle}
             >
+                <img src='../../test-images/logo.svg'/>
                 <Input
                     placeholder='username'
+                    _placeholder={{ color: "#FEFAE0" }}
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
+                    focusBorderColor="#D4A373"
+                    backgroundColor= "rgba(99, 116, 79, 0.7)"
+                    color= "#FEFAE0"
                 />
                 <Input
                     placeholder='password'
+                    _placeholder={{ color: "#FEFAE0" }}
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    focusBorderColor="#D4A373"
+                    backgroundColor= "rgba(99, 116, 79, 0.7)"
+                    color= "#FEFAE0"
                 />
                 {errorMsg && (
                     <Text color="red.500" textAlign="center">
                         {errorMsg}
                     </Text>
                 )}
-                <Button onClick={validateData}>log in</Button>
+                <Button onClick={validateData} backgroundColor="#63744F" color="#FEFAE0">log in</Button>
                 <Text textAlign="center"> {/* ensures text is centered */}
                     don't have an account?
                     <Link to={'/signup'}> sign up</Link> {/* ensures the button links to the log-in page */}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button, Flex, Grid, Input, Text, useDisclosure } from '@chakra-ui/react';
 import { supabase, profiles, userposts } from '../database/db';
-import NavigationBar from '../components/NavigationBar';
-import MenuButton from '../components/MenuButton';
+import backgroundImage from "../../test-images/gradient_bkgd.png";
 
 export default function Signup() {
     const [username, setUsername] = useState('');
@@ -13,6 +12,20 @@ export default function Signup() {
     const [errorMsg, setErrorMsg] = useState('');
     
     const navigate = useNavigate();
+
+    const backgroundStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100vw",
+    };
+
+    const inputBoxStyle = {
+        borderRadius: "25px",
+        background: "#FEFAE0",
+        padding: "20px",
+    };
 
     async function postData() {
         // user can't enter a blank form
@@ -50,40 +63,58 @@ export default function Signup() {
 
     return (
         // items in container are stacked vertically, 
-        <Flex direction="column" align="center" justify="center" height="100vh" padding={4}>
+        <Flex direction="column" align="center" justify="center" height="100vh" padding={4} style={backgroundStyle}>
             <Grid 
                 templateColumns="repeat(1, 1fr)" // creating 1 column, column will take up full available width 
                 gap={4} // space between grid items vertically and horizontally
                 width="100%" // sets the width of the grid container to 100% of the flex container's width
                 maxWidth="400px" // limits how wide the grid can become
                 justifyItems="center" // centers the content of each grid cell horizontally
+                style={inputBoxStyle}
             >
+                <img src='../../test-images/logo.svg'/>
                 <Input
                     placeholder='username'
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
+                    _placeholder={{ color: "#FEFAE0" }}
+                    focusBorderColor="#D4A373"
+                    backgroundColor= "rgba(99, 116, 79, 0.7)"
+                    color= "#FEFAE0"
                 />
                 <Input
                     placeholder='password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
+                    _placeholder={{ color: "#FEFAE0" }}
+                    focusBorderColor="#D4A373"
+                    backgroundColor= "rgba(99, 116, 79, 0.7)"
+                    color= "#FEFAE0"
                 />
                 <Input
                     placeholder='email'
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
+                    _placeholder={{ color: "#FEFAE0" }}
+                    focusBorderColor="#D4A373"
+                    backgroundColor= "rgba(99, 116, 79, 0.7)"
+                    color= "#FEFAE0"
                 />
                 <Input
                     placeholder='phone number'
                     value={phoneNum}
                     onChange={(e) => setPhoneNum(e.target.value)}
+                    _placeholder={{ color: "#FEFAE0" }}
+                    focusBorderColor="#D4A373"
+                    backgroundColor= "rgba(99, 116, 79, 0.7)"
+                    color= "#FEFAE0"
                 />
                 {errorMsg && (
                     <Text color="red.500" textAlign="center">
                         {errorMsg}
                     </Text>
                 )}
-                <Button onClick={postData}>sign up</Button>
+                <Button onClick={postData} backgroundColor="#63744F" color="#FEFAE0">sign up</Button>
                 <Text textAlign="center"> {/* ensures text is centered */}
                     already have an account?
                     <Link to={'/login'}> log in</Link> {/* ensures the button links to the log-in page */}
